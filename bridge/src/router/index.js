54213f96859de1a9c0137794f8ded77f521b7b7c
Ava's DAO <support@avasdao.org>
2022-11-06 17:18:52 -0500

    Add new logo.

--- bridge/src/router/index.js
+++ bridge/src/router/index.js
@@ -1,22 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import HelpView from '../views/HelpView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
       component: HomeView
     },
     {
       path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      component: AboutView
+    },
+    {
+      path: '/help',
+      component: HelpView
+    },
   ]
 })
 
