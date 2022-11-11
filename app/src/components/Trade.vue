77125066485ec056cc26fe6847f154728f78c2a3
Ava's DAO <support@avasdao.org>
2022-11-11 01:49:15 -0500

    Update trade window.

--- app/src/components/Trade.vue
+++ app/src/components/Trade.vue
@@ -1,58 +1,109 @@
 <template>
-    <div class="component">
+    <main class="component">
         <div class="component-header text-warning">Buy | Sell</div>
 
         <div class="component-tabs nav-header">
             <ul class="nav" role="tablist">
                 <li class="nav-item">
-                    <a role="tab" data-toggle="tab" href="#buy" class="active nav-link" aria-controls="buy" aria-selected="true">Buy Order</a>
+                    <a role="tab" data-toggle="tab" href="#buy" class="active nav-link" aria-controls="buy" aria-selected="true">
+                        Buy Order
+                    </a>
                 </li>
 
                 <li class="nav-item">
-                    <a role="tab" data-toggle="tab" href="#sell" class="nav-link" aria-controls="sell" aria-selected="false">Sell Order</a>
+                    <a role="tab" data-toggle="tab" href="#sell" class="nav-link" aria-controls="sell" aria-selected="false">
+                        Sell Order
+                    </a>
                 </li>
             </ul>
         </div>
 
         <div class="tab-content">
+            <!-- Start buy panel -->
             <div role="tabpanel" class="tab-pane fade show active" id="buy">
                 <form>
                     <div class="form-group">
-                        <label for="buyAmount">Amount of {{tokenSymbol}} tokens to buy</label>
+                        <label for="buyAmount">
+                            Amount of {{tokenSymbol}} tokens to buy
+                        </label>
+
                         <div class="input-group">
-                            <input type="number" class="form-control form-control-sm" id="buyAmount" v-model="buyAmount">
+                            <input
+                                type="number"
+                                class="form-control form-control-sm"
+                                id="buyAmount"
+                                v-model="buyAmount"
+                            >
+
                             <div class="input-group-append">
-                                <span class="input-group-text">{{tokenSymbol}}</span>
+                                <span class="input-group-text">
+                                    {{tokenSymbol}}
+                                </span>
                             </div>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="buyPrice">Price per {{tokenSymbol}} token</label>
+                        <label for="buyPrice">
+                            Price per {{tokenSymbol}} token
+                        </label>
+
                         <div class="input-group">
-                            <input type="number" class="form-control form-control-sm" id="buyPrice" v-model="buyPrice">
+                            <input
+                                type="number"
+                                class="form-control form-control-sm"
+                                id="buyPrice"
+                                v-model="buyPrice"
+                                @blur="updateBuyTotal"
+                            >
+
                             <div class="input-group-append">
-                                <span class="input-group-text">USDT</span>
+                                <span class="input-group-text">
+                                    USDT
+                                </span>
                             </div>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="buyTotal">Total order value</label>
+                        <label for="buyTotal">
+                            Total order value
+                        </label>
+
                         <div class="input-group">
-                            <input type="text" id="buyTotal" class="form-control form-control-sm" :value="buyTotal" readonly>
+                            <input
+                                type="number"
+                                id="buyTotal"
+                                class="form-control form-control-sm"
+                                v-model="buyTotal"
+                                @blur="updateBuyPrice"
+                            >
+
                             <div class="input-group-append">
-                                <span class="input-group-text">USDT</span>
+                                <span class="input-group-text">
+                                    USDT
+                                </span>
                             </div>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="buyExpires" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="The number of Ethereum blocks until the order automatically expires. (14 seconds per block.)">Order expiration time</label>
+                        <label for="buyExpires" data-toggle="tooltip" data-placement="bottom" title="">
+                            Order expiration time
+                        </label>
+
                         <div class="input-group">
-                            <input type="text" class="form-control form-control-sm" id="buyExpires" :value="expirationTime" readonly>
+                            <input
+                                type="text"
+                                class="form-control form-control-sm"
+                                id="buyExpires"
+                                :value="expirationTime" readonly
+                            >
+
                             <div class="input-group-append">
-                                <span class="input-group-text">{{expirationMeasure}}</span>
+                                <span class="input-group-text">
+                                    {{expirationMeasure}}
+                                </span>
                             </div>
                         </div>
                     </div>
@@ -60,49 +111,103 @@
                     <span class="text-warning"></span>
 
                     <div class="form-group">
-                        <button type="button" class="btn btn-success btn-sm btn-block" @click="buyOrder">Create Buy Order</button>
+                        <button
+                            type="button"
+                            class="btn btn-success btn-sm btn-block"
+                            @click="buyOrder"
+                        >
+                            Create Buy Order
+                        </button>
                     </div>
                 </form>
-            </div> <!-- tabpanel -->
+            </div>
+            <!-- End buy panel -->
 
+            <!-- Start sell panel -->
             <div role="tabpanel" class="tab-pane fade" id="sell">
                 <form>
                     <div class="form-group">
-                        <label for="sellAmount">Amount of {{tokenSymbol}} tokens to sell</label>
+                        <label for="sellAmount">
+                            Amount of {{tokenSymbol}} tokens to sell
+                        </label>
+
                         <div class="input-group">
-                            <input type="number" class="form-control form-control-sm" id="sellAmount" v-model="sellAmount">
+                            <input
+                                type="number"
+                                class="form-control form-control-sm"
+                                id="sellAmount"
+                                v-model="sellAmount"
+                            >
+
                             <div class="input-group-append">
-                                <span class="input-group-text">{{tokenSymbol}}</span>
+                                <span class="input-group-text">
+                                    {{tokenSymbol}}
+                                </span>
                             </div>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="sellPrice">Price per {{tokenSymbol}} token</label>
+                        <label for="sellPrice">
+                            Price per {{tokenSymbol}} token
+                        </label>
+
                         <div class="input-group">
-                            <input type="number" class="form-control form-control-sm" id="sellPrice" v-model="sellPrice">
+                            <input
+                                type="number"
+                                class="form-control form-control-sm"
+                                id="sellPrice"
+                                v-model="sellPrice"
+                                @blur="updateSellTotal"
+                            >
+
                             <div class="input-group-append">
-                                <span class="input-group-text">USDT</span>
+                                <span class="input-group-text">
+                                    USDT
+                                </span>
                             </div>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="sellTotal">Total order value</label>
+                        <label for="sellTotal">
+                            Total order value
+                        </label>
+
                         <div class="input-group">
-                            <input type="text" class="form-control form-control-sm" id="sellTotal" :value="sellTotal" readonly>
+                            <input
+                                type="number"
+                                class="form-control form-control-sm"
+                                id="sellTotal"
+                                v-model="sellTotal"
+                                @blur="updateSellPrice"
+                            >
+
                             <div class="input-group-append">
-                                <span class="input-group-text">USDT</span>
+                                <span class="input-group-text">
+                                    USDT
+                                </span>
                             </div>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="sellExpires" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="The number of Ethereum blocks until the order automatically expires. (14 seconds per block.)">Order expiration time</label>
+                        <label for="sellExpires" data-toggle="tooltip" data-placement="bottom" title="">
+                            Order expiration time
+                        </label>
+
                         <div class="input-group">
-                            <input type="text" class="form-control form-control-sm" id="sellExpires" :value="expirationTime" readonly>
+                            <input
+                                type="text"
+                                class="form-control form-control-sm"
+                                id="sellExpires"
+                                :value="expirationTime" readonly
+                            >
+
                             <div class="input-group-append">
-                                <span class="input-group-text">{{expirationMeasure}}</span>
+                                <span class="input-group-text">
+                                    {{expirationMeasure}}
+                                </span>
                             </div>
                         </div>
                     </div>
@@ -110,15 +215,21 @@
                     <span class="text-warning"></span>
 
                     <div class="form-group">
-                        <button type="button" class="btn btn-danger btn-sm btn-block" @click="sellOrder">Create Sell Order</button>
+                        <button
+                            type="button"
+                            class="btn btn-danger btn-sm btn-block"
+                            @click="sellOrder"
+                        >
+                            Create Sell Order
+                        </button>
                     </div>
                 </form>
 
-            </div> <!-- tabpanel -->
+            </div>
+            <!-- End sell panel -->
 
-        </div> <!-- tab-content -->
-
-    </div> <!-- component -->
+        </div>
+    </main>
 </template>
 
 <script>
@@ -126,23 +237,57 @@
 import { mapActions, mapGetters } from 'vuex'
 
 /* Import modules. */
-import numeral from 'numeral'
+// import numeral from 'numeral'
 
 export default {
-    props: {
-        // msg: String
-    },
     data: () => {
         return {
             token: null,
             buyAmount: null,
             buyPrice: null,
+            buyTotal: null,
             sellAmount: null,
             sellPrice: null,
+            sellTotal: null,
 
             expirationTime: null,
             expirationMeasure: null,
         }
+    },
+    watch: {
+        buyAmount: function (_amount) {
+            if (!_amount) {
+                return null
+            }
+
+            if (this.buyPrice) {
+                // this.buyTotal = numeral(_amount * this.buyPrice).format('0.00')
+                this.buyTotal = (_amount * this.buyPrice)
+            }
+
+            if (this.buyTotal) {
+                // this.buyTotal = numeral(buyTotal / _amount).format('0.00')
+                this.buyPrice = (this.buyTotal / _amount)
+            }
+        },
+
+        // buyPrice: function (_price) {
+        //     if (!_price || !this.buyAmount) {
+        //         return null
+        //     }
+
+        //     // this.buyTotal = numeral(_price * this.buyAmount).format('0.00')
+        //     this.buyTotal = (_price * this.buyAmount).toFixed(8)
+        // },
+
+        // buyTotal: function (_total) {
+        //     if (!_total || !this.buyAmount) {
+        //         return null
+        //     }
+
+        //     // this.buyPrice = numeral(_total / this.buyAmount).format('0.00')
+        //     this.buyPrice = (_total / this.buyAmount).toFixed(8)
+        // },
     },
     computed: {
         ...mapGetters([
@@ -169,21 +314,21 @@ export default {
             return this.token.symbol
         },
 
-        buyTotal() {
-            if (!this.buyAmount || !this.buyPrice) {
-                return null
-            }
+        // buyTotal() {
+        //     if (!this.buyAmount || !this.buyPrice) {
+        //         return null
+        //     }
 
-            return numeral(this.buyAmount * this.buyPrice).format('$0.00')
-        },
+        //     return numeral(this.buyAmount * this.buyPrice).format('$0.00')
+        // },
 
-        sellTotal() {
-            if (!this.sellAmount || !this.sellPrice) {
-                return null
-            }
+        // sellTotal() {
+        //     if (!this.sellAmount || !this.sellPrice) {
+        //         return null
+        //     }
 
-            return numeral(this.sellAmount * this.sellPrice).format('$0.00')
-        },
+        //     return numeral(this.sellAmount * this.sellPrice).format('$0.00')
+        // },
 
     },
     methods: {
@@ -199,14 +344,49 @@ export default {
             // console.log('Ready to sell')
         },
 
+        updateBuyPrice() {
+            if (!this.buyTotal || !this.buyAmount) {
+                return null
+            }
+
+            // this.buyPrice = numeral(_total / this.buyAmount).format('0.00')
+            this.buyPrice = (this.buyTotal / parseFloat(this.buyAmount)).toFixed(8)
+        },
+
+        updateBuyTotal() {
+            if (!this.buyPrice || !this.buyAmount) {
+                return null
+            }
+
+            // this.buyTotal = numeral(_price * this.buyAmount).format('0.00')
+            this.buyTotal = (this.buyPrice * parseFloat(this.buyAmount))
+        },
+
+        updateSellPrice() {
+            if (!this.sellTotal || !this.sellAmount) {
+                return null
+            }
+
+            // this.sellPrice = numeral(_total / this.sellAmount).format('0.00')
+            this.sellPrice = (this.sellTotal / parseFloat(this.sellAmount)).toFixed(8)
+        },
+
+        updateSellTotal() {
+            if (!this.sellPrice || !this.sellAmount) {
+                return null
+            }
+
+            // this.sellTotal = numeral(_price * this.sellAmount).format('0.00')
+            this.sellTotal = (this.sellPrice * parseFloat(this.sellAmount))
+        },
     },
     created: async function () {
         /* Retrieve current token. */
         this.token = await this.getToken('0x505A442B3E3E9AEDF06D54572a295F8D64f8F582')
         // console.log('CURRENT TOKEN', this.token)
 
-        this.expirationTime = 24
-        this.expirationMeasure = 'hours'
+        this.expirationTime = 7
+        this.expirationMeasure = 'days'
     },
     mounted: function () {
         //
