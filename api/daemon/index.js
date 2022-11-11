68d5a676cb73646ea5cf0b32f26c9c6a730efdcd
Ava's DAO <support@avasdao.org>
2022-11-10 23:23:57 -0500

    Add IPFS & OrbitDB to API daemon.

--- api/daemon/index.js
+++ api/daemon/index.js
@@ -1,292 +1,78 @@
-/* Import modules. */
-const Client = require('bitcoin-core')
-const moment = require('moment')
-const nodemailer = require('nodemailer')
-const PouchDB = require('pouchdb')
-const util = require('util')
-const { v4: uuidv4 } = require('uuid')
-
-/* Initialize databases. */
-// const logsDb = new PouchDB(`http://${process.env.COUCHDB_AUTH}@localhost:5984/logs`)
-// const sessionsDb = new PouchDB(`http://${process.env.COUCHDB_AUTH}@localhost:5984/sessions`)
-const notifsDb = new PouchDB('dbs/notifs')
-const sessionsDb = new PouchDB('dbs/sessions')
-
-/* Initialize new Bitcoin client. */
-const client = new Client({
-    port: process.env.NEXA_RPC_PORT || 7227, // Testnet RPC port is 7229
-    host: process.env.NEXA_RPC_HOST || '127.0.0.1',
-    username: process.env.NEXA_RPC_USER || 'user',
-    password: process.env.NEXA_RPC_PASS || 'password',
-})
-
-
-
-/**
- * Notifications
- */
-const notifs = async function (req, res) {
-    let id
-    let address
-    let body
-    let response
-    let results
-
-    body = req.body
-    console.log('BODY', body)
-
-    /* Validate body. */
-    if (!body) {
-        /* Set status. */
-        res.status(400)
-
-        /* Return error. */
-        return res.json({
-            error: 'Missing body parameter.'
-        })
-    }
-
-    address = body.address
-    console.log('\nNotification address:', address)
-
-    result = await client.validateAddress(address)
-    console.log('\nIs address valid:', result.isvalid, result)
-
-    /* Validate address. */
-    if (!result.isvalid) {
-        /* Set status. */
-        res.status(400)
-
-        /* Return error. */
-        return res.json({
-            error: 'Your Nexa address is invalid.'
-        })
-    }
-
-
-
-/*
-
-addmultisigaddress
-
-backupwallet
-
-bumpfee
-clearbanned
-
-combinepsbt
-combinerawtransaction
-converttopsbt
-
-createmultisig
-createpsbt
-createrawtransaction
-
-decodepsbt
-decoderawtransaction
-decodescript
-
-deriveaddresses
-
-dumpprivkey
-dumptxoutset
-dumpwallet
-
-encryptwallet
-estimatefee
-estimatepriority
-estimatesmartfee
-estimatesmartpriority
-
-finalizepsbt
-fundrawtransaction
-
-getaccount
-getaccountaddress
-getaddressinfo
-getbalance
-getbalances
-
-getbestblockhash
-getblockcount
-getblockfilter
-getblockhash
-getblockheader
-getblockstats
-getblocktemplate
-getblockchaininfo
-
-getchaintips
-getchaintxstats
-getdifficulty
-gethashespersec
-
-getindexinfo
-getinfo
-getmemoryinfo
-
-getmempoolancestors
-getmempooldescendants
-getmempoolentry
-getmempoolinfo
-
-getmininginfo
-
-getnettotals
-getnetworkhashps
-getnetworkinfo
-
-getnewaddress
-getnodeaddresses
-getpeerinfo
-
-getrawchangeaddress
-getrawmempool
-getrawtransaction
-getreceivedbyaccount
-getreceivedbyaddress
-getreceivedbylabel
-
-getrpcinfo
-gettransaction
-gettxout
-gettxoutproof
-gettxoutsetinfo
-
-getunconfirmedbalance
-getwalletinfo
-
-getwork
-getzmqnotifications
-
-importaddress
-importmulti
-importprivkey
-importprunedfunds
-importpubkey
-importwallet
-
-joinpsbts
-
-keypoolrefill
-
-listaccounts
-listaddressgroupings
-listbanned
-listlabels
-
-listlockunspent
-listreceivedbyaccount
-listreceivedbyaddress
-listreceivedbylabel
-listsinceblock
-listtransactions
-
-listunspent
-listwalletdir
-
-listwallets
-loadwallet
-
-lockunspent
-
-preciousblock
-prioritisetransaction
-pruneblockchain
-
-scantxoutset
-sendfrom
-sendmany
-sendrawtransaction
-sendtoaddress
-
-sethdseed
-setlabel
-
-setnetworkactive
-settxfee
-setwalletflag
-
-signmessage
-signmessagewithprivkey
-signrawtransaction
-signrawtransactionwithkey
-signrawtransactionwithwallet
-
-submitblock
-testmempoolaccept
-
-unloadwallet
-uptime
-utxoupdatepsbt
-
-validateaddress
-verifychain
-verifymessage
-verifytxoutproof
-walletcreatefundedpsbt
-walletlock
-walletpassphrase
-walletpassphrasechange
-walletprocesspsbt
-
-*/
-
-    const balance = await client.getBalance('*', 0)
-    console.log('\nBALANCE', balance)
-
-    // client.getInfo().then((help) => console.log(help))
-return res.json({ status: 'done!', balance, })
-
-    /* Generate id. */
-    id = uuidv4()
-
-    /* Add record to database. */
-    response = await notifsDb
-        .put({
-            _id: id,
-            ...body,
-        })
-        .catch(err => {
-            console.error(err)
-
-            return res.json(err)
-        })
-
-    const msgFrom = '"Nexa Rocks" <notify@nexa.rocks>'
-
-    const msgRecipient = 'info@avasdao.org'
-
-    const msgSubject = 'Nexa Rocks! Event'
-
-    const msgDetails = {
-        txid: 'd465b82e9d9e74a19b5ea0ac09308be93be8e5f3b46ad8ceb0da99005b7e9b2e',
-    }
-
-    // create reusable transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS,
-        },
-    })
-
-    // send mail with defined transport object
-    const info = await transporter.sendMail({
-        from: msgFrom,
-        to: msgRecipient,
-        subject: msgSubject,
-        text: require('../templates/plaintext')(msgDetails),
-        html: require('../templates/html')(msgDetails),
-    })
-    console.log('Message sent: %s', info.messageId)
-
-    /* Send response back to client. */
-    res.json({
-        databaseId: response.id,
-        messageId: info.messageId,
-    })
+console.log('\n  Starting Nexa Exchange Sandbox Tests..\n')
+
+/* Initialize IPFS options. */
+const ipfsOptions = {
+    EXPERIMENTAL: {
+        pubsub: true,
+    },
+    repo : './ipfs',
 }
+
+async function main () {
+    /* Initialize local variables. */
+    let pkg
+    let response
+    let result
+
+    /* Import ESM modules. */
+    const { create } = await import('ipfs')
+    const OrbitDB = await import('orbit-db')
+
+    try {
+        /* Create IPFS instance. */
+        const ipfs = await create(ipfsOptions)
+
+        /* Create OrbitDB instance. */
+        const orbitdb = await OrbitDB.default
+            .createInstance(ipfs)
+
+        /* Initialize Orbit (Orders) database. */
+        const ordersDb = await orbitdb.docs('exchange.nexa.orders')
+
+        ordersDb.events.on('load.progress', (address, hash, entry, progress, total) => {
+            console.log('  EVENT-address', address)
+            console.log('  EVENT-hash', hash)
+            // console.log('  EVENT-entry', entry)
+            console.log('  EVENT-progress', progress)
+            console.log('  EVENT-total', total)
+        })
+
+        ordersDb.events.on('ready', (dbname, heads) => {
+            console.log('  EVENTS-dbname', dbname)
+            // console.log('  EVENTS-heads', heads)
+        })
+
+        ordersDb.events.on('write', (address, entry, heads) => {
+            console.log('  EVENTS-address', address)
+            // console.log('  EVENTS-entry', entry)
+            // console.log('  EVENTS-heads', heads)
+        })
+
+        /* Initialize Orbit (Messages) database. */
+        const messagesDb = await orbitdb.docs('exchange.nexa.messages')
+
+        /* Load (Orders) database. */
+        await ordersDb.load()
+
+        // pkg = { _id: 'hello world', doc: 'all the things', followers: 3 }
+        // response = await ordersDb.put(pkg)
+
+        // pkg = { _id: 'hello universe', doc: 'all things', followers: 10 }
+        // response = await ordersDb.put(pkg)
+
+        // pkg = { _id: 'sup world', doc: 'other things', followers: 5 }
+        // response = await ordersDb.put(pkg)
+
+        result = ordersDb.get('')
+        console.log(result)
+
+        // result = await ordersDb.del('sup world')
+
+        const pending = ordersDb.query(doc => doc.followers >= 5)
+        console.log(pending)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+main()
