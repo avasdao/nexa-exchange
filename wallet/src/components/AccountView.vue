ac88585d4f1901c1d4516d29d48d6ac1f572de5e
Ava's DAO <support@avasdao.org>
2022-11-23 16:28:24 -0500

    Add poster.

---
+++ wallet/src/components/AccountView.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <h1>Balance All</h1>
+
+    </main>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        //
+    }),
+    computed: {
+        //
+    },
+    methods: {
+        //
+    },
+    created: function () {
+        //
+    },
+    mounted: function () {
+        //
+    },
+}
+</script>
