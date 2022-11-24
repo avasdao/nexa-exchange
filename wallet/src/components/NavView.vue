05aa03f14bbd964a63fe9154bacb7551299ee88e
Ava's DAO <support@avasdao.org>
2022-11-24 00:44:52 -0500

    Update receive UI.

--- wallet/src/components/NavView.vue
+++ wallet/src/components/NavView.vue
@@ -14,7 +14,10 @@
                     d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
                 />
             </svg>
-            <span class="truncate text-gray-800 text-lg">Account</span>
+
+            <span class="truncate text-gray-800 text-lg">
+                My Portfolio
+            </span>
         </router-link>
 
         <router-link to="/send" class="text-gray-900 hover:text-gray-900 hover:bg-gray-50 group rounded-md px-3 py-2 flex items-center text-sm font-medium">
--- wallet/src/views/ReceiveView.vue
+++ wallet/src/views/ReceiveView.vue
@@ -6,6 +6,8 @@ import { ref } from 'vue'
 
 import QRCode from 'qrcode'
 
+const DONATION_ADDRESS = 'nexa:nqtsq5g5v80h5ky86u5uc8uuyjqtsmxgn9rver6hzhqx6qt9'
+
 /* Initialize data URL. */
 let dataUrl = ref('')
 
@@ -23,15 +25,26 @@ const generateQR = async text => {
 }
 
 /* Generate code. */
-generateQR('nexa:')
+generateQR(DONATION_ADDRESS)
 </script>
 
 <template>
     <main>
         <NavView />
 
-        <h1>Receive</h1>
+        <h1 class="my-5 ml-3 text-3xl text-yellow-700 font-medium">
+            Receive
+        </h1>
 
-        <img :src="dataUrl" class="w-48 h-48" />
+        <img
+            :src="dataUrl"
+            class="m-5 w-48 h-48"
+        />
+
+        <h2 class="mt-3 text-xl">
+            {{DONATION_ADDRESS}}
+        </h2>
+
+        <small>Development / donation address</small>
     </main>
 </template>
