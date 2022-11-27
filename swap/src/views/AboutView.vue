be3c774be91316164c523580f0d0112bab615fb1
Ava's DAO <support@avasdao.org>
2022-11-27 03:07:32 -0500

    Add about page.

--- swap/src/views/AboutView.vue
+++ swap/src/views/AboutView.vue
@@ -31,47 +31,68 @@
         <div class="relative px-4 sm:px-6 lg:px-8">
             <div class="mx-auto max-w-prose text-lg">
                 <h1>
-                    <span class="block text-center text-lg font-semibold text-indigo-600">Introducing</span>
-                    <span class="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">JavaScript for Beginners</span>
+                    <span class="block text-center text-lg font-semibold text-indigo-600">
+                        About
+                    </span>
+
+                    <span class="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+                        Intro to Nexa Swap
+                    </span>
                 </h1>
+
                 <p class="mt-8 text-xl leading-8 text-gray-500">
-                    Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget. Eleifend egestas
-                    fringilla sapien.
+                    Nexa Swap offers the Nexa community a quick and simple platform to exchange $NEX for ANY other crypto in the global marketplace.
                 </p>
             </div>
+
             <div class="prose prose-lg prose-indigo mx-auto mt-6 text-gray-500">
                 <p>
-                    Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong> sed <strong>eget risus enim</strong>. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus
-                    varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id.
+                    Nexa Swap is an "INSTANT" exchange; which means <strong>3 Key Benefits</strong> for the average user:
                 </p>
+
                 <ul role="list">
-                    <li>Quis elit egestas venenatis mattis dignissim.</li>
-                    <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-                    <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+                    <li>5-seconds <em>(or less)</em> for asset delivery</li>
+                    <li>No KYC data for hackers to steal</li>
+                    <li>100% non-custodial — your keys, your coins</li>
                 </ul>
+
                 <p>
-                    Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet, mattis. Nunc purus, diam
-                    commodo tincidunt turpis. Amet, duis sed elit interdum dignissim.
+                    The safety and security of your assets is our #1 priority.
+                    We enforce the strictest key management policies, including: multi-signature, SGX enclaves and biometric security.
                 </p>
-                <h2>From beginner to expert in 30 days</h2>
+
+                <!-- <h2>From beginner to expert in 30 days</h2>
+
                 <p>
                     Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris,
                     ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam.
                 </p>
+
                 <blockquote>
-                    <p>Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.</p>
+                    <p>
+                        Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.
+                    </p>
                 </blockquote>
-                <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.</p>
+
+                <p>
+                    Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.
+                </p>
+
                 <figure>
                     <img class="w-full rounded-lg" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3" alt="" width="1310" height="873" />
                     <figcaption>Sagittis scelerisque nulla cursus in enim consectetur quam.</figcaption>
                 </figure>
+
                 <h2>Everything you need to get up and running</h2>
+
                 <p>
-                    Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>. Amet, massa quam varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut viverra ridiculus non molestie.
+                    Purus morbi dignissim senectus mattis <a href="javascript://">adipiscing</a>. Amet, massa quam varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut viverra ridiculus non molestie.
                     Gravida quis fringilla amet eget dui tempor dignissim. Facilisis auctor venenatis varius nunc, congue erat ac. Cras fermentum convallis quam.
                 </p>
-                <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.</p>
+
+                <p>
+                    Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.
+                </p> -->
             </div>
         </div>
     </div>
--- swap/src/views/HelpView.vue
+++ swap/src/views/HelpView.vue
@@ -5,12 +5,14 @@ let showFaq1 = ref(true)
 let showFaq2 = ref(false)
 let showFaq3 = ref(false)
 let showFaq4 = ref(false)
+let showFaq5 = ref(false)
 
 const closeAll = () => {
     showFaq1.value = false
     showFaq2.value = false
     showFaq3.value = false
     showFaq4.value = false
+    showFaq5.value = false
 }
 const toggle1 = () => {
     closeAll()
@@ -28,11 +30,15 @@ const toggle4 = () => {
     closeAll()
     showFaq4.value = !showFaq4.value
 }
+const toggle5 = () => {
+    closeAll()
+    showFaq5.value = !showFaq5.value
+}
 
 </script>
 
 <template>
-    <main class="bg-gray-50">
+    <main class="bg-gray-100 border-2 border-gray-300 rounded-lg">
         <div class="mx-auto max-w-7xl py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
             <div class="mx-auto max-w-3xl divide-y-2 divide-gray-200">
                 <h2 class="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
@@ -130,6 +136,31 @@ const toggle4 = () => {
                             <p class="text-base text-gray-500">
                                 During this "Alpha Preview" phase, we ONLY support "buying" $NEX.
                                 If you wish to sell $NEX, please visit our <a href="https://app.nexa.exchange" target="_blank">Nexa Pro Exchange</a>.
+                            </p>
+                        </dd>
+                    </div>
+
+                    <div class="pt-6">
+                        <dt class="text-lg">
+                            <!-- Expand/collapse question button -->
+                            <button @click="toggle5" type="button" class="flex w-full items-start justify-between text-left text-gray-400" aria-controls="faq-0" aria-expanded="false">
+                                <span class="font-medium text-gray-900">
+                                    Is the displayed exchange rate fixed?
+                                </span>
+
+                                <span class="ml-6 flex h-7 items-center">
+                                    <svg class="h-6 w-6 transform" :class="[ showFaq5 ? 'rotate-0' : '-rotate-180' ]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </dt>
+
+                        <dd class="mt-2 pr-12" id="faq-1" v-if="showFaq5">
+                            <p class="text-base text-gray-500">
+                                NO!
+                                The exchange rate will adjust based on "real-time" market conditions.
+                                To view the LIVE marketplace, please visit our <a href="https://app.nexa.exchange" target="_blank">Nexa Pro Exchange</a>.
                             </p>
                         </dd>
                     </div>
