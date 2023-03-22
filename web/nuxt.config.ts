64b5e488b69f5e7f9133fd950d124572905f810d
Ava's DAO <support@avasdao.org>
2023-03-22 19:46:59 -0400

    Add NexaJS + db support.

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,6 +1,40 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    /* Application Settings */
+    app: {
+        /* Application Header */
+        head: {
+            title: 'Nexa Exchange — Your Keys. Your Coins.',
+            meta: [
+                { charset: 'utf-8' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                { hid: 'description', name: 'description', content: 'A dedicated Nexa exchange.' },
+                { name: 'format-detection', content: 'telephone=no' }
+            ],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            ],
+            script: [
+                { src: '/js/matomo.js' },
+            ],
+        },
+    },
+
+    /* Modules */
     modules: [
+        /* Tailwind CSS */
         '@nuxtjs/tailwindcss',
+
+        /* Pinia */
+        '@pinia/nuxt',
     ],
+
+    /* Route Rules */
+    routeRules: {
+        /* Disable server-side rendering for Admin area. */
+        '/admin/**': { ssr: false },
+
+        /* Add CORS headers to API. */
+        '/api/**': { cors: true },
+    },
 })
