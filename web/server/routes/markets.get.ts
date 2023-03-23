130e4f408773f510a1dfe6a87678d37cf646c8fc
Ava's DAO <support@avasdao.org>
2023-03-23 14:59:23 -0400

    Add markets endpoint.

--- web/server/routes/markets.get.ts
+++ web/server/routes/markets.get.ts
@@ -1,9 +1,37 @@
-export default defineEventHandler((event) => {
+/* Import modules. */
+import moment from 'moment'
+
+export default defineEventHandler(async (event) => {
     /* Set 24-hour volume. */
     // FIXME FOR DEV PURPOSES ONLY
     const vol_24 = 1234567890
 
-    return {
+    const price = await $fetch('/price')
+
+    const summary = {
+        price,
+    }
+
+    /* Set updated timestamp. */
+    const updatedAt = moment().unix()
+
+    /* Build CoinMarketCap market. */
+    const COIN_MARKET_CAP = {
         vol_24,
+        updatedAt,
+    }
+
+    /* Build Txbit market. */
+    const TXBIT = {
+        vol_24,
+        updatedAt,
+    }
+
+    /* Return ALL markets. */
+    return {
+        summary,
+        COIN_MARKET_CAP,
+        TXBIT,
+        updatedAt,
     }
 })
