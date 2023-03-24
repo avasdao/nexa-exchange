19154f838d290af93f1a075d8c4c40918405d6b7
Ava's DAO <support@avasdao.org>
2023-03-24 16:32:24 -0400

    Local PubSub.

--- graphql/src/subscriptions/block.js
+++ graphql/src/subscriptions/block.js
@@ -12,8 +12,28 @@ import {
     GraphQLString,
 } from 'graphql'
 
-export default (_pubsub) => {
+import { PubSub } from 'graphql-subscriptions'
+/* Initialize PubSub. */
+const pubsub = new PubSub()
+
+// FOR DEV PURPOSES ONLY
+let counter = 1337
+const SAMPLE_BLOCK = {
+  "hash": "78ee2c10c94e377a56c2d25e6478d75b3168043dec6a4bfaabc73421a03df8aa",
+  "confirmations": 1,
+  "height": 0,
+}
+
+// FOR DEV PURPOSES ONLY
+setInterval(() => {
+    pubsub.publish('NEW_BLOCK', { block: {
+        ...SAMPLE_BLOCK,
+        height: counter++,
+    } })
+}, 5000)
+
+export default {
     type: BlockType,
-    subscribe: () => _pubsub.asyncIterator(['NEW_BLOCK']),
+    subscribe: () => pubsub.asyncIterator(['NEW_BLOCK']),
     description: `A Block subscription will report every new block that appears on the blockchain.`,
 }
