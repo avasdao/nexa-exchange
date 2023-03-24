dbdd7854f1812f855a2e44c9efeedc6284febedc
Ava's DAO <support@avasdao.org>
2023-03-24 10:33:36 -0400

    Add WebSocket support to GraphQL.

--- graphql/server.js
+++ graphql/server.js
@@ -1,24 +1,24 @@
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import express from 'express'
-import http from 'http'
 import rateLimit from 'express-rate-limit'
 import PouchDB from 'pouchdb'
 import { WebSocketServer } from 'ws'
 
 import { buildSchema } from 'graphql'
 import { createHandler } from 'graphql-http/lib/use/express'
+import { createServer } from 'http'
 import { useServer } from 'graphql-ws/lib/use/ws'
+
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
 
 import schema from './src/schema.js'
 import typeDefs from './src/typeDefs.js'
 import resolvers from './src/resolvers.js'
 
 
-
-import { ApolloServer } from '@apollo/server'
-import { expressMiddleware } from '@apollo/server/express4'
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
 
 // import express from 'express';
 // import cors from 'cors';
@@ -62,43 +62,59 @@ const limiter = rateLimit({
 // app.set('trust proxy', 3) // NOTE: 0 is localhost, 1,2 are Cloudflare
 // app.get('/ip', (request, response) => response.send(request.ip))
 
-const httpServer = http.createServer(app)
+const httpServer = createServer(app)
+
+
+// Creating the WebSocket server
+const wsServer = new WebSocketServer({
+    // This is the `httpServer` we created in a previous step.
+    server: httpServer,
+
+    // Pass a different path here if app.use
+    // serves expressMiddleware at a different path
+    path: '/graphql',
+})
+
+// Hand in the schema we just created and have the
+// WebSocketServer start listening.
+const serverCleanup = useServer({ schema }, wsServer);
 
 
 const server = new ApolloServer({
-    // typeDefs,
-    // resolvers,
     schema,
     plugins: [
+        // Proper shutdown for the HTTP server.
         ApolloServerPluginDrainHttpServer({ httpServer }),
+
+        // Proper shutdown for the WebSocket server.
+        {
+            async serverWillStart() {
+                return {
+                    async drainServer() {
+                        await serverCleanup.dispose()
+                    },
+                }
+            },
+        },
     ],
 })
 
-// Ensure we wait for our server to start
+
 await server.start()
+app.use('/graphql', cors(), bodyParser.json(), expressMiddleware(server))
 
+// Now that our HTTP server is fully set up, we can listen to it.
+httpServer.listen(PORT, () => {
+    console.log(`Server is now running on http://localhost:${PORT}/graphql`);
+})
 
-app.use('/', cors(),
-    // 50mb is the limit that `startStandaloneServer` uses, but you may configure this to suit your needs
-    bodyParser.json({ limit: '50mb' }),
-
-    // expressMiddleware accepts the same arguments:
-    // an Apollo Server instance and optional configuration options
-    expressMiddleware(server, {
-        context: async ({ req }) => ({ token: req.headers.token }),
-    }),
-)
-
-// Modified server startup
-await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve))
-console.log(`🚀 Server ready at http://localhost:${PORT}/`);
-
-
-// const server = new WebSocketServer({
-//   	port: 7000,
-//   	path: '/socket',
-// })
-// useServer({ schema }, server)
-// console.log('Listening for GraphQL /Socket requests on port 7000')
-
-// ------------------------
+// app.use('/', cors(),
+//     // 50mb is the limit that `startStandaloneServer` uses, but you may configure this to suit your needs
+//     bodyParser.json({ limit: '50mb' }),
+//
+//     // expressMiddleware accepts the same arguments:
+//     // an Apollo Server instance and optional configuration options
+//     expressMiddleware(server, {
+//         context: async ({ req }) => ({ token: req.headers.token }),
+//     }),
+// )
