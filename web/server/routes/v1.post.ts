2bbd8407fd93f64de417d8f0228e79e2dcf55eb2
Ava's DAO <support@avasdao.org>
2023-06-17 20:53:28 -0400

    Update post endpoint.

--- web/server/routes/v1.post.ts
+++ web/server/routes/v1.post.ts
@@ -1,7 +1,35 @@
 export default defineEventHandler(async (event) => {
+    let body
+    let errors
+    let method
+    let pkg
+    let success
+
     /* Set (request) body. */
-    const body = await readBody(event)
+    body = await readBody(event)
     console.log('BODY', body)
 
-    return body
+    if (body?.method) {
+        method = body.method
+    }
+
+    if (method) {
+        switch(method) {
+        case 'createOrder':
+            success = 'creating new order...'
+            break
+        default:
+            errors = 'Unknown method'
+        }
+    }
+
+    errors = null
+
+    pkg = {
+        success,
+        body,
+        errors,
+    }
+
+    return pkg
 })
