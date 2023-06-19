d8ac7f00fe9b9d367aedca9f9ebe5f69439bad5f
Ava's DAO <support@avasdao.org>
2023-06-19 04:15:09 -0400

    Add swap amount.

--- swap/components/GiftCardOrder.vue
+++ swap/components/GiftCardOrder.vue
@@ -49,7 +49,7 @@ const props = defineProps({
                 type="text"
                 name="price"
                 id="price"
-                class="block w-full rounded-md border-0 py-1.5 pl-7 pr-12 text-2xl text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+                class="block w-full rounded-md border-0 py-1.5 pl-10 pr-12 text-2xl text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
                 placeholder="1 - 2000"
                 aria-describedby="price-currency"
             />
--- swap/components/MobileSwap.vue
+++ swap/components/MobileSwap.vue
@@ -61,14 +61,18 @@ const openScanner = async () => {
                 </NuxtLink>.
             </p>
 
-            <section v-if="Swap.isShowingNexa">
-                <h2 class="text-2xl font-medium">
-                    Nexa
-                </h2>
+            <section v-if="Swap.isShowingNexa" class="flex justify-between">
+                <div>
+                    <h2 class="text-gray-700 text-2xl font-medium uppercase">
+                        Nexa
+                    </h2>
 
-                <p>
-                    Nexa is a PoW blockchain with the capability to process 100K TPS using hardware scaling technology.
-                </p>
+                    <p class="text-sm text-gray-500">
+                        Nexa is a PoW blockchain with the capability to process 100K TPS using hardware scaling technology.
+                    </p>
+                </div>
+
+                <img src="~/assets/nexa.svg" class="w-28 h-auto" />
             </section>
 
             <section v-if="!Swap.isShowingNexa">
@@ -113,7 +117,6 @@ const openScanner = async () => {
                         />
                     </ul>
                 </nav>
-
             </section>
         </section>
 
@@ -149,7 +152,7 @@ const openScanner = async () => {
                     @click="openScanner"
                     :disabled="Swap.isValidAddress"
                 >
-                    <svg class="w-12 h-12 text-yellow-700" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z"></path></svg>
+                    <svg class="w-12 h-auto text-yellow-700" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z"></path></svg>
                 </button>
             </div>
 
@@ -173,57 +176,68 @@ const openScanner = async () => {
             />
         </section>
 
-        <section
-            v-if="Swap.isShowingNexa"
-            class="my-5 p-5 bg-gradient-to-r from-sky-200 to-sky-300 border-4 border-sky-400 rounded-lg shadow-lg"
-            :class="[ Swap.isValidAddress ? 'opacity-100' : 'opacity-30 cursor-not-allowed']"
-        >
-            <h1 class="text-4xl font-bold">
-                I have:
+        <div v-if="Swap.isShowingNexa" class="mx-10 my-3 border-t border-gray-300" />
+
+        <section v-if="Swap.isShowingNexa" class="mx-3 my-3 flex flex-col gap-3">
+            <p class="px-3 text-sm text-gray-500">
+                Please enter the dollar value of <span class="text-indigo-500 font-medium">$NEXA</span> that you want to receive.
+                <span class="block text-red-500 font-medium text-center italic">
+                    Maximum swap amount is $1,000
+                </span>
+            </p>
+
+            <div class="relative mt-2 rounded-md shadow-sm">
+                <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                    <span class="text-2xl text-gray-500">$</span>
+                </div>
+
+                <input
+                    type="text"
+                    name="price"
+                    id="price"
+                    class="block w-full rounded-md border-2 border-yellow-500 py-1.5 pl-10 pr-12 text-2xl text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+                    placeholder="1 - 1000"
+                    aria-describedby="price-currency"
+                />
+
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                    <span class="text-2xl text-gray-500" id="price-currency">USD</span>
+                </div>
+            </div>
+        </section>
+
+        <section v-if="Swap.isShowingNexa" class="mx-3">
+            <h1 class="mt-3 text-4xl text-gray-600 font-bold">
+                I have ↴
             </h1>
 
-            <nav class="mt-5 flex flex-col sm:flex-row">
-                <button class="mr-3 flex items-center group cursor-not-allowed" @click="showPrev">
-                    <span class="ml-1 mb-3 text-7xl text-sky-400">
-                        ‹
-                    </span>
-                    <span class="text-xs text-sky-400 font-bold">
-                        SHOW<br />PREV
-                    </span>
-                </button>
-
-                <ul class="flex flex-col sm:flex-row gap-5">
+            <nav class="mt-5 flex flex-col">
+                <ul class="grid grid-cols-2 gap-3">
                     <AssetButton
-                        @click="Swap.startUsdt"
+                        @click="openUsdtSelect"
                         assetid="USDT"
                         asset-name="Tether"
-                        class="w-full sm:w-36"
-                        :class="[ Swap.isValidAddress ? 'opacity-100' : 'opacity-50 cursor-not-allowed']"
+                        class="w-full bg-gradient-to-b from-green-700 to-green-900"
                     />
 
                     <AssetButton
-                        @click="Swap.startBch"
                         assetid="BCH"
                         asset-name="Bitcoin Cash"
-                        class="w-full sm:w-36"
-                        :class="[ Swap.isValidAddress ? 'opacity-100' : 'opacity-50 cursor-not-allowed']"
+                        class="w-full bg-gradient-to-b from-lime-600 to-lime-800"
                     />
 
                     <AssetButton
-                        assetid="BTC"
-                        asset-name="Bitcoin"
-                        class="w-full sm:w-36 opacity-50 cursor-not-allowed"
+                        assetid="CASH"
+                        asset-name="Cash Money"
+                        class="w-full bg-gradient-to-b from-teal-600 to-teal-800"
+                    />
+
+                    <AssetButton
+                        assetid="DASH"
+                        asset-name="Dash"
+                        class="w-full bg-gradient-to-b from-sky-700 to-sky-900"
                     />
                 </ul>
-
-                <button class="ml-3 flex items-center group" @click="showMore">
-                    <span class="text-xs font-bold group-hover:text-cyan-600">
-                        SHOW<br />MORE
-                    </span>
-                    <span class="ml-1 mb-3 text-7xl group-hover:text-cyan-400">
-                        ›
-                    </span>
-                </button>
             </nav>
         </section>
 
