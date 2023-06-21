5559d74a523c3abff7b9b3bcce3753610e93c578
Ava's DAO <support@avasdao.org>
2023-06-21 09:52:56 -0400

    Enable order request.

--- web/server/routes/v1/order/[...orderid].get.ts
+++ web/server/routes/v1/order/[...orderid].get.ts
@@ -1,9 +1,39 @@
-export default defineEventHandler((event) => {
-    /* Set (invalid) path URL. */
-    const orderid = event.context.params.orderid
+/* Set endpoint. */
+const API_ENDPOINT = 'https://api.telr.io/v1/exchange'
 
-    return {
-        orderid,
-        msg: 'lets go!',
+export default defineEventHandler(async (event) => {
+    /* Initialize locals. */
+    let body
+    let headers
+    let method
+    let orderid
+
+    /* Set (invalid) path URL. */
+    orderid = event.context.params.orderid
+
+    body = {
+        method: 'requestOrder',
+        params: {
+            orderid,
+        }
     }
+
+    /* Validate order id. */
+    if (orderid) {
+        /* Set method. */
+        method = 'POST'
+
+        /* Set headers. */
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-TELR-API': process.env.TELR_API_KEY,
+        }
+
+        return await $fetch(API_ENDPOINT, {
+            method,
+            headers,
+            body,
+        })
+        .catch(err => console.error(err))
 })
