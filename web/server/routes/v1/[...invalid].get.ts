95dd64f4bdbac787c76d7b8f8c5c33eb937cb51e
Ava's DAO <support@avasdao.org>
2023-08-08 12:17:31 -0400

    Fix API error reporting.

--- web/server/routes/v1/[...invalid].get.ts
+++ web/server/routes/v1/[...invalid].get.ts
@@ -1,9 +1,19 @@
+import { log } from "console"
+
 export default defineEventHandler(async (event) => {
     /* Set (invalid) path URL. */
     const path = event.context.params.invalid
 
+    let response
+
     if (path.startsWith('ticker/price') || path.startsWith('ticker/quote')) {
-        return await $fetch(`https://api.telr.io/v1/${path}`)
+        response = await $fetch(`https://api.telr.io/v1/${path}`)
+            .catch(err => console.error(err))
+        console.log('TICKER RESPONSE', response)
+    }
+
+    if (response) {
+        return response
     }
 
     return {
