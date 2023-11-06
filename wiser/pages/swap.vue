7e69904a924707c049d308619f78096df79df4ba
Ava's DAO <support@avasdao.org>
2023-11-06 01:32:49 -0500

    Fix responsive layout.

--- wiser/pages/swap.vue
+++ wiser/pages/swap.vue
@@ -22,14 +22,14 @@ const System = useSystemStore()
 </script>
 
 <template>
-    <main class="w-screen grid grid-cols-7 gap-20 overflow-hidden bg-gradient-to-b from-yellow-500 to-yellow-800">
-        <section class="w-full col-span-4 bg-lime-50 flex justify-center items-center">
+    <main class="w-screen grid grid-cols-1 lg:grid-cols-7 gap-20 overflow-hidden bg-gradient-to-b from-yellow-500 to-yellow-800">
+        <section class="w-full lg:col-span-4 bg-lime-50 flex justify-center items-center position">
             <div class="p-10 bg-lime-50 border-2 border-lime-200 rounded-2xl shadow-md">
                 <ChartsBar />
             </div>
         </section>
 
-        <section class="col-span-3">
+        <section class="lg:col-span-3 order-first lg:order-none">
             <SwapWindow />
         </section>
     </main>
