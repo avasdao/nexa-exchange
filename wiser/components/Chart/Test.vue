6c459da5e6c7301d18153b6fce78e794fdf9a9f1
Ava's DAO <support@avasdao.org>
2023-11-08 02:09:15 -0500

    Add Apex charts lib.

---
+++ wiser/components/Chart/Test.vue
@@ -0,0 +1,27 @@
+<script setup>
+const chartOptions = {
+  chart: {
+    id: "vuechart-example",
+  },
+  xaxis: {
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+  },
+};
+const series = [
+  {
+    name: "series-1",
+    data: [30, 40, 35, 50, 49, 60, 70, 91],
+  },
+];
+</script>
+
+<template>
+    <ClientOnly>
+        <apexchart
+            width="500"
+            type="bar"
+            :options="chartOptions"
+            :series="series"
+        ></apexchart>
+    </ClientOnly>
+</template>
--- wiser/pages/swap.vue
+++ wiser/pages/swap.vue
@@ -25,7 +25,7 @@ const System = useSystemStore()
     <main class="w-screen grid grid-cols-1 lg:grid-cols-7 gap-20 overflow-hidden bg-gradient-to-b from-yellow-500 to-yellow-800">
         <section class="w-full lg:col-span-4 bg-lime-50 flex justify-center items-center position">
             <div class="p-10 bg-lime-50 border-2 border-lime-200 rounded-2xl shadow-md">
-                <ChartsBar />
+                <ChartTest />
             </div>
         </section>
 
