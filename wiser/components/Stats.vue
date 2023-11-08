1b47d6c6b1fa584c7612b2c48e777175a5f113e6
Ava's DAO <support@avasdao.org>
2023-11-08 14:02:37 -0500

    Update stats.

--- wiser/components/Stats.vue
+++ wiser/components/Stats.vue
@@ -36,9 +36,18 @@ const props = defineProps({
             </div>
 
             <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
-                <h2 class="text-base font-semibold leading-8 text-indigo-400">Our track record</h2>
-                <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Trusted by thousands of creators&nbsp;worldwide</p>
-                <p class="mt-6 text-lg leading-8 text-gray-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+                <h2 class="text-lg font-semibold leading-8 text-indigo-400 tracking-widest">
+                    Your Keys. Your Coins. Since Block #0
+                </h2>
+
+                <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                    Trusted by thousands of Creators, Traders and Builders worldwide...
+                </p>
+
+                <p class="mt-6 text-lg leading-8 text-gray-300">
+                    Our track record speaks for itself, as do our legion of loyal supporters on a regular basis.
+                    We remain focused on <span class="font-bold">ONE</span> goal; working tirelessly to continuously deliver you the <span class="font-bold">MOST</span> value in all of crypto.
+                </p>
             </div>
 
             <dl class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 text-white sm:mt-20 sm:grid-cols-2 sm:gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-4">
