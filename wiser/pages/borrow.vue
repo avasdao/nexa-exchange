4dc5bb0e4c2f4d83f7f12e6419eac1686f7e3c4b
Ava's DAO <support@avasdao.org>
2023-11-07 23:08:48 -0500

    Add stats section.

--- wiser/pages/borrow.vue
+++ wiser/pages/borrow.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+/* Import modules. */
+import numeral from 'numeral'
+
 useHead({
-    title: `Lend — WiserSwap`,
+    title: `Borrow — WiserSwap`,
     meta: [
         { name: 'description', content: `Nexa Studio makes building your next BIG idea effortless.` }
     ],
@@ -10,10 +13,21 @@ useHead({
 import { useSystemStore } from '@/stores/system'
 const System = useSystemStore()
 
-// onMounted(() => {
-//     console.log('Mounted!')
-//     // Now it's safe to perform setup operations.
-// })
+const lenders = ref(null)
+
+const init = async () => {
+    lenders.value = await $fetch('/api/lenders')
+        .catch(err => console.error(err))
+    console.log('LENDERS', lenders.value)
+}
+
+const totalValueLocked = (_token) => {
+    return '$133.70'
+}
+
+onMounted(() => {
+    init()
+})
 
 // onBeforeUnmount(() => {
 //     console.log('Before Unmount!')
@@ -22,14 +36,101 @@ const System = useSystemStore()
 </script>
 
 <template>
-    <main class="max-w-6xl mx-auto px-3 py-5 flex flex-col gap-5">
+    <main class="max-w-6xl mx-auto px-3 py-5 flex flex-col gap-4">
         <h1 class="text-5xl font-medium">
             Borrow
         </h1>
 
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eius voluptatem minus natus at eveniet dolorum eos mollitia, maxime animi excepturi harum omnis illum odit recusandae pariatur! Unde, explicabo molestias.
-        </p>
+        <div class="px-4 sm:px-6 lg:px-8">
+
+            <div class="-mx-4 mt-8 flow-root sm:mx-0">
+                <table class="min-w-full">
+                    <colgroup>
+                        <col class="w-full sm:w-1/2" />
+                        <col class="sm:w-1/6" />
+                        <col class="sm:w-1/6" />
+                        <col class="sm:w-1/6" />
+                    </colgroup>
+
+                    <thead class="border-b border-gray-300 text-gray-900">
+                        <tr>
+                            <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-lg font-semibold text-gray-900 sm:pl-0">
+                                Lender Overview
+                            </th>
+
+                            <th scope="col" class="hidden px-3 py-3.5 text-center text-lg font-semibold text-gray-900 sm:table-cell">
+                                Rating
+                            </th>
+
+                            <th scope="col" class="hidden px-3 py-3.5 text-center text-lg font-semibold text-gray-900 sm:table-cell">
+                                APY%
+                            </th>
+
+                            <th scope="col" class="py-3.5 pl-3 pr-4 text-right text-lg font-semibold text-gray-900 sm:pr-0">
+                                24h Volume
+                            </th>
+
+                            <th scope="col" class="py-3.5 pl-3 pr-4 text-right text-lg font-semibold text-gray-900 sm:pr-0">
+                                &nbsp;
+                            </th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr v-for="token of lenders" :key="token.id" class="border-b border-gray-200">
+                            <td class="max-w-0 py-5 pl-4 pr-3 text-lg sm:pl-0">
+                                <div class="flex flex-row gap-3">
+                                    <img :src="token.iconUrl" class="h-16 w-auto" />
+
+                                    <section>
+                                        <div class="font-medium text-gray-900 text-2xl tracking-wider">
+                                            {{token.title}}
+                                        </div>
+
+                                        <p class="mt-1 flex flex-row gap-2 items-baseline">
+                                            <span class="text-gray-500 text-xs uppercase">
+                                                Owner
+                                            </span>
+
+                                            <NuxtLink to="https://twitter.com/0xShomari" target="_blank" class="text-base text-blue-500 hover:text-blue-400">
+                                                0xShomari
+                                            </NuxtLink>
+                                        </p>
+
+                                        <p class="mt-1 text-gray-500 text-xs">
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla veritatis rerum ipsam dolorum iste iure hic cum ab illum obcaecati ea ullam enim, corporis, corrupti repudiandae eius non atque natus!
+                                        </p>
+                                    </section>
+                                </div>
+                            </td>
+
+                            <td class="hidden px-3 py-5 text-center text-lg text-gray-500 sm:table-cell">
+                                ${{token.ticker}}
+                            </td>
+
+                            <td class="hidden px-3 py-5 text-center text-lg text-gray-500 sm:table-cell">
+                                {{totalValueLocked(token)}}
+                            </td>
+
+                            <td class="py-5 pl-3 pr-4 text-right text-lg text-gray-500 sm:pr-0">
+                                $80.88
+                            </td>
+
+                            <td class="py-5 pl-6 pr-4 sm:pr-0 flex flex-col gap-3">
+                                <button class="w-full px-5 py-2 bg-green-600 text-green-50 text-xl font-medium rounded-lg shadow hover:bg-sky-400">
+                                    Farm
+                                </button>
+
+                                <NuxtLink :to="'https://nexa.exchange/token/' + token.id" target="_blank" class="w-full px-5 py-2 bg-amber-500 text-amber-50 text-xl font-medium rounded-lg shadow hover:bg-amber-400">
+                                    Details
+                                </NuxtLink>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     </main>
 
     <Footer />
