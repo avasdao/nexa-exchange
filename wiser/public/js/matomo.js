a196ea7f788013aa922190f101c06803b6df41f9
Ava's DAO <support@avasdao.org>
2023-11-13 00:57:35 -0500

    Update analytics.

--- wiser/public/js/matomo.js
+++ wiser/public/js/matomo.js
@@ -5,7 +5,7 @@ _paq.push(['enableLinkTracking']);
 (function() {
 var u="//matomo.avasdao.org/";
 _paq.push(['setTrackerUrl', u+'matomo.php']);
-_paq.push(['setSiteId', '18']);
+_paq.push(['setSiteId', '19']);
 var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
