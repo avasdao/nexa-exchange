b8df26c6cbb806fe7646ef34e0ef0d8f68c009e4
Ava's DAO <support@avasdao.org>
2023-11-19 19:44:44 -0500

    Add debugging.

--- web/server/routes/v1/session/[...sessionid].get.ts
+++ web/server/routes/v1/session/[...sessionid].get.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (event) => {
     /* Set session id. */
     const sessionid = event.context.params.sessionid
-    // console.log('SESSION ID', sessionid)
+    console.log('SESSION ID', sessionid)
 
     /* Return (Telr Exchange) session. */
     return await $fetch('https://api.telr.io/v1/exchange/session/' + sessionid)
