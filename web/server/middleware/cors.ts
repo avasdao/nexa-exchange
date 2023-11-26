aa0a788652cb806be5ff19de97d84eb1894c59d9
Ava's DAO <support@avasdao.org>
2023-11-26 05:08:10 -0500

    Add CORS middleware.

---
+++ web/server/middleware/cors.ts
@@ -0,0 +1,16 @@
+export default defineEventHandler((event) => {
+    setResponseHeaders(event, {
+        'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Expose-Headers': '*',
+    })
+
+    if (getMethod(event) === 'OPTIONS') {
+        event.node.res.statusCode = 204
+        event.node.res.statusMessage = 'No Content.'
+
+        return 'OK'
+    }
+})
