c7db2dc10d34281d714c52e73c6ee3461073844e
Ava's DAO <support@avasdao.org>
2023-12-21 15:41:29 -0500

    Update token description.

--- web/pages/token/index.vue
+++ web/pages/token/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 useHead({
-    title: `$NXL — Nexa Exchange`,
+    title: `$NXL — Nexa Exchange Loyalty`,
     meta: [
         { name: 'description', content: `Nexa Exchange.` }
     ],
@@ -53,23 +53,15 @@ const System = useSystemStore()
                             </h1>
 
                             <p class="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
-                                Official burning &amp; staking token of Nexa Studio.
+                                Official burning &amp; staking token of Nexa Exchange.
                             </p>
 
                             <p class="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
-                                Providing Nexicans who dream BIG with a premium suite of tools and services to Plan, Build and maximize a sustainable Profit!
+                                Investors burn $NXL to pay exchange fees and/or stake $NXL to activate premium tools & services, featuring: real-time market insights, exclusive alpha metrics & security assessments, plus priority access to Ava's world-class support team.
                             </p>
 
                             <p class="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
-                                Builders burn & stake $STUDIO Time for access to premium tools & services, featuring: bleeding-edge AI, uncensorable content storage & delivery, plus a direct line to Ava's world-class support team.
-                            </p>
-
-                            <p class="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
-                                Creators may choose to publish one or more of their original NFTs and SFTs to The Nexican Gallery — Nexa's premier, ultra-exclusive, digital art collection.
-                            </p>
-
-                            <p class="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
-                                Builder's & Creator's works are their own.
+                                Each token is equal to one vote in ALL "official" Nexa Exchange DAO quorums.
                             </p>
                         </div>
 
@@ -177,5 +169,57 @@ const System = useSystemStore()
             </div>
 
         </div>
+
+        <!-- <section class="bg-white py-24 sm:py-32">
+            <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                <div class="mx-auto grid max-w-2xl grid-cols-1 gap-8 overflow-hidden lg:mx-0 lg:max-w-none lg:grid-cols-4">
+                    <div>
+                        <time datetime="2021-08" class="flex items-center text-sm font-semibold leading-6 text-indigo-600">
+                            <svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
+                                <circle cx="2" cy="2" r="2" fill="currentColor" />
+                            </svg>
+                            Aug 2021
+                            <div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
+                        </time>
+                        <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Founded company</p>
+                        <p class="mt-1 text-base leading-7 text-gray-600">Nihil aut nam. Dignissimos a pariatur et quos omnis. Aspernatur asperiores et dolorem dolorem optio voluptate repudiandae.</p>
+                    </div>
+                    <div>
+                        <time datetime="2021-12" class="flex items-center text-sm font-semibold leading-6 text-indigo-600">
+                            <svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
+                                <circle cx="2" cy="2" r="2" fill="currentColor" />
+                            </svg>
+                            Dec 2021
+                            <div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
+                        </time>
+                        <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Secured $65m in funding</p>
+                        <p class="mt-1 text-base leading-7 text-gray-600">Provident quia ut esse. Vero vel eos repudiandae aspernatur. Cumque minima impedit sapiente a architecto nihil.</p>
+                    </div>
+                    <div>
+                        <time datetime="2022-02" class="flex items-center text-sm font-semibold leading-6 text-indigo-600">
+                            <svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
+                                <circle cx="2" cy="2" r="2" fill="currentColor" />
+                            </svg>
+                            Feb 2022
+                            <div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
+                        </time>
+                        <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Released beta</p>
+                        <p class="mt-1 text-base leading-7 text-gray-600">Sunt perspiciatis incidunt. Non necessitatibus aliquid. Consequatur ut officiis earum eum quia facilis. Hic deleniti dolorem quia et.</p>
+                    </div>
+                    <div>
+                        <time datetime="2022-12" class="flex items-center text-sm font-semibold leading-6 text-indigo-600">
+                            <svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
+                                <circle cx="2" cy="2" r="2" fill="currentColor" />
+                            </svg>
+                            Dec 2022
+                            <div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
+                        </time>
+                        <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Global launch of product</p>
+                        <p class="mt-1 text-base leading-7 text-gray-600">Ut ipsa sint distinctio quod itaque nam qui. Possimus aut unde id architecto voluptatem hic aut pariatur velit.</p>
+                    </div>
+                </div>
+            </div>
+        </section> -->
+
     </main>
 </template>
