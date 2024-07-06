70409e525a5719a7e5d2ebb776ade2a4d9e83432
Ava's DAO <support@avasdao.org>
2024-07-06 00:22:02 -0400

    Fix debug.

--- wiser/nuxt.config.ts
+++ wiser/nuxt.config.ts
@@ -48,6 +48,7 @@ export default defineNuxtConfig({
             API_ENDPOINT: 'https://nexa.exchange/v1',
             API_ENDPOINT_RAW: process.env.API_ENDPOINT,
             API_ENDPOINT_NUXT: process.env.NUXT_ENV_API_ENDPOINT,
+            API_ENDPOINT_TEST: process.env.NUXT_ENV_API_ENDPOINT_TEST,
         },
     },
 
